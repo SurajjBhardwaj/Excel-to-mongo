adc52220171e69d61f4eaa4fa8ea173bc4f12ea7
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2023-09-01 19:21:22 +0530

    Database connected

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-// express server using dotenv
 require("dotenv").config();
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const URI = process.env.URI;
 
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Database is connected successfully`);
+  })
+  .catch((err) => {
+    console.error(`Error in connecting to the database: ${err}`);
+  });
 
 app.get("/", (req, res) => {
-    
-    res.send("heyy balak");
-    
+  res.send("heyy balak");
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Backend server is running at http://localhost${PORT}`);
-})
+  console.log(`Backend server is running at http://localhost:${PORT}`);
+});
